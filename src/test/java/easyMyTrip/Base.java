e385c6c84e93845ad95ef3b05754b4149334fba5
surendravj <vadaparthysurendra@gmail.com>
2021-09-29 14:49:18 +0530

    Added base driver code

--- src/test/java/easyMyTrip/Base.java
+++ src/test/java/easyMyTrip/Base.java
@@ -1,5 +1,37 @@
 package easyMyTrip;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Base {
+
+	Properties props = new Properties();
+	WebDriver driver;
+
+	public void intilizeDriver(String browser) throws IOException{
+		switch (browser.toUpperCase()) {
+		case "GOOGLE":
+			System.setProperty("webdriver.chrome.driver", "src//test//resources//chromedriver.exe");
+			driver = new ChromeDriver();
+			break;
+
+		case "EDGE":
+			System.setProperty("webdriver.edge.driver", "src//test//resources//msedgedriver.exe");
+			driver = new EdgeDriver();
+			break;
+		case "FIREFOX":
+			System.setProperty("webdriver.gecko.driver", "src//test//resources//geckodriver.exe");
+			driver = new FirefoxDriver();
+			break;
+		}
+		FileReader reader=new FileReader("src//test//resources//config.properties");
+		props.load(reader);
+	}
 
 }
--- src/test/java/easyMyTrip/Login.java
+++ src/test/java/easyMyTrip/Login.java
@@ -2,8 +2,40 @@ package easyMyTrip;
 
 
 
+
+
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 public class Login extends Base {
-  
-  
- 
+
+	@FindBy(id = "txtEmail")
+	private WebElement username;
+	
+	@FindBy(name = "btn_Login")
+	private WebElement continueButton;
+	
+	@BeforeClass
+	public void beforeClass() throws IOException {
+		intilizeDriver("google");
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	public void test() {
+		driver.get(props.getProperty("url"));
+	}
+	
+	@AfterClass
+	public void afterclass()
+	{
+		driver.quit();
+	}
+
 }
