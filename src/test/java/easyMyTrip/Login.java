53ea8be9bdce0c1a456ad274b3477ff12d890632
surendravj <vadaparthysurendra@gmail.com>
2021-09-30 09:52:33 +0530

    Added login

--- src/test/java/easyMyTrip/Login.java
+++ src/test/java/easyMyTrip/Login.java
@@ -2,8 +2,15 @@ package easyMyTrip;
 
 import java.io.IOException;
 
-import org.openqa.selenium.support.PageFactory;
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class Login extends Base {
 
@@ -12,5 +19,64 @@ public class Login extends Base {
 		intilizeDriver(props.getProperty("browser"));
 		PageFactory.initElements(driver, this);
 	}
-	
+
+	By my_account = By.cssSelector("div.sub_header_link3");
+	By log_in = By.cssSelector("a.btn_loginnew");
+	By in_data = By.id("txtEmail");
+	By cont = By.cssSelector("input#shwotp");
+	By use_pass = By.id("shwlgnbx");
+	By pass_word = By.id("txtEmail2");
+	By logged_in = By.xpath("//*[@id=\"emailgnBox\"]/div[1]/div[2]/div/div[8]/input");
+
+	@SuppressWarnings("deprecation")
+	@BeforeClass
+	public void beforeClass() {
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get(props.getProperty("url"));
+	}
+
+	public void fillNumber() {
+		driver.findElement(my_account).click();
+		driver.findElement(log_in).click();
+		driver.findElement(in_data).sendKeys(props.getProperty("mobileNumber2"));
+		driver.findElement(cont).click();
+	}
+
+	@Test
+	public void test1() throws InterruptedException {
+		fillNumber();
+		driver.findElement(use_pass).click();
+		driver.findElement(pass_word).sendKeys(props.getProperty("loginPassword"));
+		driver.findElement(logged_in).click();
+		System.out.println("Login Successful");
+	}
+
+	@Test
+	public void test2() throws InterruptedException {
+		driver.findElement(my_account).click();
+		driver.findElement(log_in).click();
+		driver.findElement(in_data).sendKeys(props.getProperty("wrongMobileNumber"));
+		WebElement invalidno = driver.findElement(By
+				.xpath("//div[@class='login']//div[@class='i-eml'][normalize-space()='* Enter a valid Phone Number']"));
+		Assert.assertEquals(invalidno.getText(), "");
+
+	}
+
+	@Test
+	public void test3() throws InterruptedException {
+		fillNumber();
+		driver.findElement(use_pass).click();
+		driver.findElement(pass_word).sendKeys(props.getProperty("wrongLoginPassword"));
+		driver.findElement(logged_in).click();
+		WebElement invalidpass = driver.findElement(By.xpath("//*[@id=\"ValidPass\"]"));
+		Assert.assertEquals(invalidpass.getText(), "");
+
+	}
+
+	@AfterClass
+	public void afterclass() {
+		driver.quit();
+	}
+
 }
